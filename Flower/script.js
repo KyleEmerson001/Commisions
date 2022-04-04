f0a8ba971a00d957dd0f7f6fda33ab80573dfde7
KyleEmerson001 <82608312+KyleEmerson001@users.noreply.github.com>
2022-04-03 21:03:44 -0700

    update

---
+++ Flower/script.js
@@ -0,0 +1,8 @@
+if(document.form.radio1.value == "one")
+<img src="free88.gif">
+
+if(document.form.radio1.value == "two")
+<img src="wobutton.gif">
+
+if(document.form.radio1.value == "three")
+<img src="but3.gif"></img>
